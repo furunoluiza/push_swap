71a9d8175470e4c8ff6a8f9861d78797406315b4
lfuruno- <luizafuruno@gmail.com>
2024-03-11 13:16:35 -0300

    Corringindo função para verificar números repetidos

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfuruno- <lfuruno-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 13:14:47 by lfuruno-          #+#    #+#             */
+/*   Updated: 2024/03/11 13:15:15 by lfuruno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
-#include <stdio.h>
 
 //check se só tem numeros, mas ver ser tem + ou -
 //check se tem mais de 11 caract
@@ -33,12 +44,19 @@ int repeat_number(int arg, long *array)
 {
 	//Comparar o valor com todos os outros, talvez são dois loops
 	int	i;
+	int	j;
 
 	i = 0;
-	while (arg-- > 0)
+	j = 1;
+	while (i < arg - 1)
 	{
-		if (arg > 1 && array[i] == array[i + 1])
-			return (1);
+		j = i + 1;
+		while (j < arg - 1)
+		{
+			if (array[i] == array [j])
+				return (1);
+			j++;
+		}
 		i++;
 	}
 	return (0);
@@ -68,7 +86,11 @@ int	main(int argc, char **argv)
 			j++;
 			i++;
 		}
-		printf("%d", repeat_number(argc, array));
+		if (repeat_number(argc, array) == 1)
+		{
+			ft_putstr_fd("Error", 1);
+			return (0);
+		}
 	}
 	return (0);
 }
